a4425993960a1447a6911b0313b93a02f8979fdd
lucas abreu <lucas2abreu@hotmail.com>
2017-03-16 13:55:10 -0300

    Revert "delete project"
    This reverts commit 05fafeb8d5cc5b8490c7b753b928f12d9e3701b5.

---
+++ IHC_Login/IHC_Login/FrmLogin.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace IHC_Login
+{
+    partial class FrmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmLogin));
+            this.lblFacaOLogin = new System.Windows.Forms.Label();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.txbUsuario = new System.Windows.Forms.TextBox();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.txbSenha = new System.Windows.Forms.TextBox();
+            this.lblCopyright = new System.Windows.Forms.Label();
+            this.lblSuporte = new System.Windows.Forms.LinkLabel();
+            this.picBoxSuporte = new System.Windows.Forms.PictureBox();
+            this.picBoxLogin2 = new System.Windows.Forms.PictureBox();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnEntrar = new System.Windows.Forms.Button();
+            this.picBoxLogin1 = new System.Windows.Forms.PictureBox();
+            this.picBoxLogin = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxSuporte)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxLogin2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxLogin1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxLogin)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblFacaOLogin
+            // 
+            this.lblFacaOLogin.AutoSize = true;
+            this.lblFacaOLogin.BackColor = System.Drawing.SystemColors.Control;
+            this.lblFacaOLogin.Font = new System.Drawing.Font("MS Reference Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFacaOLogin.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.lblFacaOLogin.Location = new System.Drawing.Point(157, 116);
+            this.lblFacaOLogin.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblFacaOLogin.Name = "lblFacaOLogin";
+            this.lblFacaOLogin.Size = new System.Drawing.Size(210, 35);
+            this.lblFacaOLogin.TabIndex = 5;
+            this.lblFacaOLogin.Text = "Faça o Login";
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Font = new System.Drawing.Font("MS Reference Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuario.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.lblUsuario.Location = new System.Drawing.Point(158, 156);
+            this.lblUsuario.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(217, 28);
+            this.lblUsuario.TabIndex = 6;
+            this.lblUsuario.Text = "Usuário ou E-mail:";
+            // 
+            // txbUsuario
+            // 
+            this.txbUsuario.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbUsuario.Location = new System.Drawing.Point(161, 188);
+            this.txbUsuario.Margin = new System.Windows.Forms.Padding(2);
+            this.txbUsuario.Name = "txbUsuario";
+            this.txbUsuario.Size = new System.Drawing.Size(233, 31);
+            this.txbUsuario.TabIndex = 0;
+            // 
+            // lblSenha
+            // 
+            this.lblSenha.AutoSize = true;
+            this.lblSenha.Font = new System.Drawing.Font("MS Reference Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSenha.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.lblSenha.Location = new System.Drawing.Point(158, 223);
+            this.lblSenha.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(91, 28);
+            this.lblSenha.TabIndex = 7;
+            this.lblSenha.Text = "Senha:";
+            // 
+            // txbSenha
+            // 
+            this.txbSenha.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbSenha.Location = new System.Drawing.Point(160, 256);
+            this.txbSenha.Margin = new System.Windows.Forms.Padding(2);
+            this.txbSenha.Name = "txbSenha";
+            this.txbSenha.Size = new System.Drawing.Size(233, 31);
+            this.txbSenha.TabIndex = 1;
+            // 
+            // lblCopyright
+            // 
+            this.lblCopyright.AutoSize = true;
+            this.lblCopyright.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCopyright.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lblCopyright.Location = new System.Drawing.Point(276, 577);
+            this.lblCopyright.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblCopyright.Name = "lblCopyright";
+            this.lblCopyright.Size = new System.Drawing.Size(693, 26);
+            this.lblCopyright.TabIndex = 8;
+            this.lblCopyright.Text = "Copyright © 2017 - PUC Minas - Todos os direitos reservados";
+            // 
+            // lblSuporte
+            // 
+            this.lblSuporte.ActiveLinkColor = System.Drawing.Color.DodgerBlue;
+            this.lblSuporte.AutoSize = true;
+            this.lblSuporte.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblSuporte.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblSuporte.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSuporte.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblSuporte.LinkColor = System.Drawing.Color.White;
+            this.lblSuporte.Location = new System.Drawing.Point(924, 56);
+            this.lblSuporte.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblSuporte.Name = "lblSuporte";
+            this.lblSuporte.Size = new System.Drawing.Size(92, 26);
+            this.lblSuporte.TabIndex = 4;
+            this.lblSuporte.TabStop = true;
+            this.lblSuporte.Text = "Suporte";
+            this.lblSuporte.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lblSuporte_LinkClicked);
+            // 
+            // picBoxSuporte
+            // 
+            this.picBoxSuporte.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.picBoxSuporte.Image = global::IHC_Login.Properties.Resources.desk_icon_28;
+            this.picBoxSuporte.Location = new System.Drawing.Point(913, 8);
+            this.picBoxSuporte.Margin = new System.Windows.Forms.Padding(2);
+            this.picBoxSuporte.Name = "picBoxSuporte";
+            this.picBoxSuporte.Size = new System.Drawing.Size(81, 46);
+            this.picBoxSuporte.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBoxSuporte.TabIndex = 12;
+            this.picBoxSuporte.TabStop = false;
+            this.picBoxSuporte.Click += new System.EventHandler(this.picBoxSuporte_Click);
+            // 
+            // picBoxLogin2
+            // 
+            this.picBoxLogin2.Image = global::IHC_Login.Properties.Resources.login;
+            this.picBoxLogin2.Location = new System.Drawing.Point(563, 135);
+            this.picBoxLogin2.Margin = new System.Windows.Forms.Padding(2);
+            this.picBoxLogin2.Name = "picBoxLogin2";
+            this.picBoxLogin2.Size = new System.Drawing.Size(271, 270);
+            this.picBoxLogin2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBoxLogin2.TabIndex = 10;
+            this.picBoxLogin2.TabStop = false;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCancelar.Font = new System.Drawing.Font("MS Reference Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.btnCancelar.Image = global::IHC_Login.Properties.Resources.btnEntrarECancelar;
+            this.btnCancelar.Location = new System.Drawing.Point(281, 303);
+            this.btnCancelar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(112, 37);
+            this.btnCancelar.TabIndex = 3;
+            this.btnCancelar.Text = "&Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnEntrar
+            // 
+            this.btnEntrar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnEntrar.Font = new System.Drawing.Font("MS Reference Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEntrar.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.btnEntrar.Image = global::IHC_Login.Properties.Resources.btnEntrarECancelar;
+            this.btnEntrar.Location = new System.Drawing.Point(157, 303);
+            this.btnEntrar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnEntrar.Name = "btnEntrar";
+            this.btnEntrar.Size = new System.Drawing.Size(108, 37);
+            this.btnEntrar.TabIndex = 2;
+            this.btnEntrar.Text = "&Entrar";
+            this.btnEntrar.UseVisualStyleBackColor = true;
+            this.btnEntrar.Click += new System.EventHandler(this.btnEntrar_Click);
+            // 
+            // picBoxLogin1
+            // 
+            this.picBoxLogin1.Location = new System.Drawing.Point(135, 93);
+            this.picBoxLogin1.Margin = new System.Windows.Forms.Padding(2);
+            this.picBoxLogin1.Name = "picBoxLogin1";
+            this.picBoxLogin1.Size = new System.Drawing.Size(737, 387);
+            this.picBoxLogin1.TabIndex = 2;
+            this.picBoxLogin1.TabStop = false;
+            // 
+            // picBoxLogin
+            // 
+            this.picBoxLogin.Image = global::IHC_Login.Properties.Resources.fundo_azulLogin;
+            this.picBoxLogin.Location = new System.Drawing.Point(-5, -1);
+            this.picBoxLogin.Margin = new System.Windows.Forms.Padding(2);
+            this.picBoxLogin.Name = "picBoxLogin";
+            this.picBoxLogin.Size = new System.Drawing.Size(1008, 608);
+            this.picBoxLogin.TabIndex = 0;
+            this.picBoxLogin.TabStop = false;
+            // 
+            // FrmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1002, 607);
+            this.ControlBox = false;
+            this.Controls.Add(this.lblSuporte);
+            this.Controls.Add(this.picBoxSuporte);
+            this.Controls.Add(this.lblCopyright);
+            this.Controls.Add(this.picBoxLogin2);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnEntrar);
+            this.Controls.Add(this.txbSenha);
+            this.Controls.Add(this.lblSenha);
+            this.Controls.Add(this.txbUsuario);
+            this.Controls.Add(this.lblUsuario);
+            this.Controls.Add(this.lblFacaOLogin);
+            this.Controls.Add(this.picBoxLogin1);
+            this.Controls.Add(this.picBoxLogin);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "FrmLogin";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.FrmLogin_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxSuporte)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxLogin2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxLogin1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxLogin)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBoxLogin;
+        private System.Windows.Forms.PictureBox picBoxLogin1;
+        private System.Windows.Forms.Label lblFacaOLogin;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.TextBox txbUsuario;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.TextBox txbSenha;
+        private System.Windows.Forms.Button btnEntrar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.PictureBox picBoxLogin2;
+        private System.Windows.Forms.Label lblCopyright;
+        private System.Windows.Forms.PictureBox picBoxSuporte;
+        private System.Windows.Forms.LinkLabel lblSuporte;
+    }
+}
+
